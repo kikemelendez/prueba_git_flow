7ab5304714d5f24d413980b3bc71c69353ea7dc2
Eduardo E Melendez Campis <eduardo.melendez@ceiba.com.co>
2018-09-10 07:20:52 -0500

    GRAL: Cambio de nombre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Parqueadero Tukana';
+  title = 'Parqueadero Unidad Residencial Tukana';
   visible = false;
   decirAdios() {
     this.visible = true;
